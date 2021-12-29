bfbabfa8022ba1ce8f9bfd07bfcc71bfab2a3af6
recepsirinn <recep.sirin@akinon.com>
2021-12-30 00:30:32 +0300

    Added contracts and optimized db around the whole services

--- payment/payment/settings.py
+++ payment/payment/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'payment.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'localhost',
+        'NAME': 'payment',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
