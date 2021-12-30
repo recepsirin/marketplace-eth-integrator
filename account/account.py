a17651211e2224f157b987a11ebc1d03fd8e47f2
recepsirinn <recep.sirin@akinon.com>
2021-12-30 03:16:13 +0300

    Added account provider

---
+++ account/account.py
@@ -0,0 +1,22 @@
+from web3 import Web3
+
+
+class CreateAccount:
+    def __init__(self, provider=None):
+        self.w3 = Web3(Web3.HTTPProvider(provider or
+                                         'https://mainnet.infura.io/v3/65ccf5b7f3454c00b5c0c65acb9cfb61'))
+
+    def _check_connection(self):
+        return self.w3.isConnected()
+
+    def create(self, encrypt: str):
+        try:
+            if self._check_connection():
+                account = self.w3.eth.account.create()
+                return {
+                    "Address": account.address,
+                    "Private Key": self.w3.toHex(account.privateKey),
+                    "Key Store": account.encrypt(encrypt)
+                }
+        except Exception as e:
+            raise e
